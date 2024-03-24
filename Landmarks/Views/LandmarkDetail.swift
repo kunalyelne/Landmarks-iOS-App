ed384aacc89fc315fbcc912b4af405b884dcb4e9
Kunal <kunal17058@gmail.com>
2024-03-24 16:28:16 +0530

    Add Landmarl Detail View and NavigationLink

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,42 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Kunal Yelne on 24/03/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
