43711505e93a1e91f4ef286cd0e11b2c7d4496a6
Kunal <kunal17058@gmail.com>
2024-04-03 23:27:28 +0530

    Add Hexagon View File

---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,17 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Kunal Yelne on 03/04/24.
+//
+
+import Foundation
+import CoreGraphics
+
+struct HexagonParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+}
