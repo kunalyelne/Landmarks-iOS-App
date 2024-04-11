47eb87dbeb9d63c0c0ede241805c63d883cecf3b
Kunal <kunal17058@gmail.com>
2024-04-11 09:36:12 +0530

    Add app icon, badge background and badge symbol

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Kunal Yelne on 11/04/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol()
+}
